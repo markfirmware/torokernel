36b5c484902ed4e1686afcca2fb6a4c3c271e7fa
markfirmware <markfirmware@users.noreply.github.com>
2017-07-20 12:43:38 +0000

    circleci cli

--- linux-testqemushm.sh
+++ linux-testqemushm.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-function docker-bash {
-    if [ "$(which $1)" != "" ]
-    then
-        eval $*
-    else
-        local DOCKER_IMAGE=markfirmware/lazarus:markfirmware-x64-1500447372285
-        local COMMAND="docker run --rm -i -v $(pwd):/workdir --entrypoint /bin/bash $DOCKER_IMAGE -c \"$*\""
-        log $COMMAND
-        eval $COMMAND
-    fi
-}
-
 function callfpc {
     header callfpc $*
-    local PROJECT=markfirmware
     local FPC="fpc -l- -v0ewn -B -Fu../rtl -Fu../rtl/drivers $*"
     log $FPC
-    (docker-bash "cd $PROJECT; $FPC") |& tee -a $LOG
+    eval $FPC |& tee -a $LOG
 }
 
 function log {
@@ -40,19 +27,22 @@ SHORTDASHES=$LONGDASHES
 LONGDASHES="$LONGDASHES$LONGDASHES"
 LONGDASHES="$LONGDASHES$LONGDASHES"
 
+cd rtl
+
 SCRIPT=linux-testqemushm.sh
-LOG=artifacts/build.log
+LOG=build.log
 mkdir artifacts
 log $(date)
 header script: $SCRIPT
 cat $SCRIPT >> $LOG
 log
 
-for f in rtl/*.pas rtl/drivers/*.pas
+for f in *.pas drivers/*.pas
 do
     callfpc -TWin64 ../$f
 done
 
+#cd markfirmware
 #callfpc build.pas
 #callfpc -TWin64 toroqemushm.pas
 
