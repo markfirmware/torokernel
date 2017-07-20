4809dbebb74e788cb65c11e7f10c4136b54e00b8
markfirmware <markfirmware@users.noreply.github.com>
2017-07-20 03:39:38 +0000

    circle.yml

---
+++ linux-testqemushm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function docker-bash {
+    if [ "$(which $1)" != "" ]
+    then
+        eval $*
+    else
+        local DOCKER_IMAGE=markfirmware/lazarus:markfirmware-x64-1500447372285
+        local COMMAND="docker run --rm -i -v $(pwd):/workdir --entrypoint /bin/bash $DOCKER_IMAGE -c \"$*\""
+        echo $COMMAND
+        eval $COMMAND
+    fi
+}
+
+function fpc {
+    local PROJECT=markfirmware
+    local FPC="fpc -l- -B -Fu../rtl -Fu../rtl/drivers $*"
+    docker-bash "cd $PROJECT; $FPC" | tee -a artifacts/build.log
+}
+
+mkdir artifacts
+fpc build.pas
+fpc -TWin64 toroqemushm.pas
+
+#/c/Program\ Files/qemu/qemu-system-x86_64 -m 512M -smp 2 -drive format=raw,file=toroqemushm.img
