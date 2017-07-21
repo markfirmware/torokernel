749d875962fc5ab667e406a6529a04f56b57a991
markfirmware <markfirmware@users.noreply.github.com>
2017-07-21 08:45:00 +0000

    .

--- linux-testqemushm.sh
+++ linux-testqemushm.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-function docker-bash {
+# run local tool if in path, otherwise call docker image
+function xdocker-bash {
     if [ "$(which $1)" != "" ]
     then
         eval $*
     else
-        local DOCKER_IMAGE=markfirmware/lazarus:markfirmware-x64-1500447372285
+        local DOCKER_IMAGE=markfirmware/torokernel-bash:fpctest-1500623771948
         local COMMAND="docker run --rm -i -v $(pwd):/workdir --entrypoint /bin/bash $DOCKER_IMAGE -c \"$*\""
         log $COMMAND
         eval $COMMAND
     fi
 }
+function docker-bash {
+        local DOCKER_IMAGE=markfirmware/torokernel-bash:fpctest-1500623771948
+        local COMMAND="docker run --rm -i -v $(pwd):/workdir --entrypoint /bin/bash $DOCKER_IMAGE -c \"$*\""
+        log $COMMAND
+        eval $COMMAND
+}
 
-function callfpc {
-    header callfpc $*
-    local PROJECT=markfirmware
+function torofpc {
+    header torofpc $*
     local FPC="fpc -l- -v0ewn -B -Furtl -Furtl/drivers $*"
     log $FPC
     docker-bash $FPC |& tee -a $LOG
@@ -31,29 +37,29 @@ function header {
     log $LONGDASHES
 }
 
-LONGDASHES=-
-LONGDASHES="$LONGDASHES$LONGDASHES"
-LONGDASHES="$LONGDASHES$LONGDASHES"
-LONGDASHES="$LONGDASHES$LONGDASHES"
-LONGDASHES="$LONGDASHES$LONGDASHES"
-SHORTDASHES=$LONGDASHES
-LONGDASHES="$LONGDASHES$LONGDASHES"
+SHORTDASHES=----------------
+LONGDASHES="$SHORTDASHES$SHORTDASHES"
 LONGDASHES="$LONGDASHES$LONGDASHES"
 
 SCRIPT=linux-testqemushm.sh
 LOG=artifacts/build.log
-mkdir artifacts
+mkdir -p artifacts
 log $(date)
 header script: $SCRIPT
 cat $SCRIPT >> $LOG
 log
 
-for f in rtl/*.pas rtl/drivers/*.pas
-do
-    callfpc -TWin64 $f
-done
+#for f in rtl/*.pas rtl/drivers/*.pas
+#do
+#   torofpc -TWin64 $f
+#done
 
-#callfpc build.pas
-#callfpc -TWin64 toroqemushm.pas
+docker-bash "nasm -o markfirmware/head64.o -f elf64 markfirmware/head64.s"
+docker-bash "fpc -B -s markfirmware/testelf.pas; cp -a link-with-multiboot.res link.res; ./ppas.sh"
+
+
+#torofpc markfirmware/toroqemushm.pas
+#torofpc build.pas
+#docker-bash fpc -i
 
 #/c/Program\ Files/qemu/qemu-system-x86_64 -m 512M -smp 2 -drive format=raw,file=toroqemushm.img
