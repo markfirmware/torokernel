1dca4685c2f3d8b74e34df3456bbed5a7f132f79
markfirmware <markfirmware@users.noreply.github.com>
2017-07-21 10:16:49 +0000

    circleci

---
+++ markfirmware/linux-testelf.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function torodocker {
+    if [ "$CIRCLECI" == "true" ]
+    then
+        eval $*
+    else
+        local DOCKER_IMAGE=markfirmware/torodocker:torodocker-1500629424965
+        local COMMAND="docker run --rm -i -v $(pwd):/workdir --entrypoint /bin/bash $DOCKER_IMAGE -c \"$*\""
+        log $COMMAND
+        eval $COMMAND |& tee -a $LOG
+    fi
+}
+
+function log {
+    echo $* | tee -a $LOG
+}
+
+function header {
+    log
+    log $LONGDASHES
+    log $SHORTDASHES $*
+    log $LONGDASHES
+}
+
+SHORTDASHES=----------------
+LONGDASHES="$SHORTDASHES$SHORTDASHES"
+LONGDASHES="$LONGDASHES$LONGDASHES"
+
+SCRIPT=linux-testelf.sh
+LOG=artifacts/build.log
+mkdir -p artifacts
+rm -f $LOG
+
+log $(date)
+
+header script: $SCRIPT
+cat $SCRIPT | tee -a $LOG
+log
+
+header building testelf.pas
+torodocker "nasm -o head64.o -f elf64 head64.s"
+torodocker "fpc -B -s testelf.pas && cp -a link-with-multiboot.res link.res && ./ppas.sh"
+mv testelf artifacts/
+
+header running qemu
+torodocker qemu-system-x86_64 -kernel testelf
+
+echo
+echo see $LOG
