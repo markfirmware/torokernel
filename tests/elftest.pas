733e2c14eddf2241cb46300b72a5d299296eeee5
markfirmware <markfirmware@users.noreply.github.com>
2017-07-24 05:12:16 +0000

    elftest - elf link toro counter  no multiboot

---
+++ tests/elftest.pas
@@ -0,0 +1,11 @@
+program ElfTest;
+
+//uses
+//System in '..\rtl\system.pas';
+
+var
+  Counter;
+begin
+  while True do
+    Inc(Counter);
+end.
---
+++ tests/elftest.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function torodocker {
+    if [ "$CIRCLECI" == "true" ]
+    then
+        log $*
+        eval $* |& tee -a $LOG
+    else
+        local DOCKER_IMAGE=markfirmware/torodocker:torodocker-1500863107533
+        local COMMAND="docker run --rm -i -v $(pwd):/workdir -p 1234:1234 --entrypoint /bin/bash $DOCKER_IMAGE -c \"$*\""
+        log $COMMAND
+        eval $COMMAND |& tee -a $LOG
+    fi
+}
+
+function log {
+    echo $* | tee -a $LOG
+}
+
+function header {
+    log
+    log $LONGDASHES
+    log $SHORTDASHES $*
+    log $LONGDASHES
+}
+
+function showfile {
+    header $1
+    cat $1 >> $LOG
+}
+
+SHORTDASHES=----------------
+LONGDASHES="$SHORTDASHES$SHORTDASHES"
+LONGDASHES="$LONGDASHES$LONGDASHES"
+
+LOG=$(pwd)/artifacts/build.log
+mkdir -p $(pwd)/artifacts
+rm -f $LOG
+
+log $(date)
+
+showfile elftest.pas
+
+header build elftest.pas
+pushd ..
+torodocker "fpc -l- -g -B -s -Fu./rtl -Fu./rtl/drivers tests/elftest.pas"
+popd
+
+echo
+echo see $LOG
